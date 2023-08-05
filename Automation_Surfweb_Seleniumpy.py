e2d6c70cc65a5ba2cd7687cf02ff4ac51e63d5ed
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2023-08-06 02:31:46 +0700

    update

--- Automation_Surfweb_Seleniumpy.py
+++ Automation_Surfweb_Seleniumpy.py
@@ -1,1 +1,9 @@
-from selenium import
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Edge()
+driver.get("https://linkler.site/2Bnrf")
+button = driver.find_element(By.ID, "csubmit")
+button.click()
+time.sleep(100)
